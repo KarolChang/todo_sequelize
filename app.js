8c5f10e5f771ade414c6a84c022168c73b378edb
Karol <super000999888@gmail.com>
2021-03-23 17:19:21 +0800

    feat: add login & register route and page

--- app.js
+++ app.js
@@ -16,6 +16,28 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+// routes for user authentication
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
+// listen server
 app.listen(PORT, () => {
   console.log(`App is listening on http://localhost:${PORT}`)
 })
